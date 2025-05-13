883e4ed5b1e579e59f83f2f9a6e95ed90dd7924f
lukas-haer <lukas.haertig@gmail.com>
2025-05-13 23:44:08 +0200

    refactor(CreateQuestion): Cleaned up some old classes

--- frontend/components/gamemaster/createQuiz/types/CreateSingleChoiseQuestion.tsx
+++ frontend/components/gamemaster/createQuiz/types/CreateSingleChoiseQuestion.tsx
@@ -1,29 +1,30 @@
-import { Component, template } from "uix/components/Component.ts";
+import { Component, template, style } from "uix/components/Component.ts";
 import { SingleChoiceQuestion } from "./SingleChoiseQuestion.ts";
 
 @template((props) => <section> 
 
-	<div>
 		<div>
-			<textarea class={"form-control"} placeholder={"Question"}> { props.question.questionText }</textarea>
-			<div class={"row"}>
-				<div class={"col-12"}>
-					<input type="text" class={"form-control"} placeholder={"Answer 1"} value={props.question.answers.a} />
+			<textarea placeholder={"Question"}> { props.question.questionText }</textarea>
+			<div>
+				<div>
+					<input type="text" placeholder="Answer 1" value={props.question.answers.a} />
 				</div>
-				<div class={"col-12"}>
-					<input type="text" class={"form-control"} placeholder={"Answer 2"} value={props.question.answers.b} />
+				<div>
+					<input type="text" placeholder="Answer 2" value={props.question.answers.b} />
 				</div>
-				<div class={"col-12"}>
-					<input type="text" class={"form-control"} placeholder={"Answer 3"} value={props.question.answers.c} />
+				<div>
+					<input type="text" placeholder="Answer 3" value={props.question.answers.c} />
 				</div>
-				<div class={"col-12"}>
-					<input type="text" class={"form-control"} placeholder={"Answer 4"} value={props.question.answers.d} />
+				<div>
+					<input type="text" placeholder="Answer 4" value={props.question.answers.d} />
 				</div>
 			</div>
 		</div>	
-	</div>
 
 </section>
+)
+@style(css`
+	`
 )
 
 export class CreateSingleChoiseQuestion extends Component<{ question: SingleChoiceQuestion }> {}
