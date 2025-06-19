ce3c911b8bd0583aca0d08a1884aedd42849e682
lukas-haer <lukas.haertig@gmail.com>
2025-06-19 18:45:05 +0200

    Join screen now swapes in components as needed and the logic is in line with the design

--- frontend/src/components/player/JoinScreen/JoinScreen.tsx
+++ frontend/src/components/player/JoinScreen/JoinScreen.tsx
@@ -1,120 +1,135 @@
-import { Datex } from "datex-core-legacy/datex.ts";
-import { ObjectRef } from "datex-core-legacy/runtime/pointers.ts";
-import { JoinGameReturn, GetCurrentQuestionReturn } from "../../../models/PlayerApiReturns.ts";
-import { getHostIdFromGamecode } from "backend/lobbyManagement/LobbyManagement.ts"
-import GameScreen from "../GameScreen/GameScreen.tsx";
+import { Datex } from 'datex-core-legacy/datex.ts';
+import { ObjectRef } from 'datex-core-legacy/runtime/pointers.ts';
+import { JoinGameReturn, GetCurrentQuestionReturn } from '../../../models/PlayerApiReturns.ts';
+import { getHostIdFromGamecode } from 'backend/lobbyManagement/LobbyManagement.ts';
+import GameScreen from '../GameScreen/GameScreen.tsx';
 import { Component, template } from 'uix/components/Component.ts';
-import { Snackbar, successSnackbarMessage, failureSnackbarMessage} from "frontend/src/components/utils/snackbar/Snackbar.tsx"
-import { LoadingScreen } from "frontend/src/components/utils/loadingscreen/LoadingScreen.tsx";
+import { Snackbar, successSnackbarMessage, failureSnackbarMessage } from 'frontend/src/components/utils/snackbar/Snackbar.tsx';
+import { LoadingScreen } from 'frontend/src/components/utils/loadingscreen/LoadingScreen.tsx';
 
 type JoinScreenProps = {
-    id?:string;
+    id?: string;
 };
 
-const stateId = $("loading");
-const currentRoundId = $("");
+const stateId = $('loading');
+const currentRoundId = $('');
 
 //TODO: can this be pulled from PlayerAPI class?
 export type PlayerAPIType = {
-  joinGame: (name: string) => JoinGameReturn;
-  getCurrentQuestion: () => GetCurrentQuestionReturn;
-  submitAnswer: (answerId: number) => number;
-}
+    joinGame: (name: string) => JoinGameReturn;
+    getCurrentQuestion: () => GetCurrentQuestionReturn;
+    submitAnswer: (answerId: number) => number;
+};
 
-const apiObj: ObjectRef<{playerApi?: PlayerAPIType}> = $({}); //encapsulate api in ObjectRef to guarantee reactivity
+const apiObj: ObjectRef<{ playerApi?: PlayerAPIType }> = $({}); //encapsulate api in ObjectRef to guarantee reactivity
 
 //TODO: error handling, user feedback (snackbar/form), form validation for endpoint format and username length
-@template<JoinScreenProps>(({id}) =>{
+@template<JoinScreenProps>(({ id }) => {
+    const gamecode = $(decodeURIComponent(id ?? ''));
+    const endpointId = $('');
+    const activeComponent = $('asdf');
+    const name = $('');
 
-  const gamecode = $(decodeURIComponent(id ?? ""));
-  const endpointId = $("")
-  const activeComponent = $("loading")
-  const name = $("");
+    async function getEndpointByGamecode() {
+        try {
+            const gameCodeRegex = /^\d{6}$/; //Checks if gamecode consists of exactly 6 numbers
 
-  async function getEndpointByGamecode() {
-          try {
-          const gameCodeRegex = /^\d{6}$/; //Checks if gamecode consists of exactly 6 numbers
-
-            if(!gamecode.val || gamecode.val == "") {
-              failureSnackbarMessage("No Gamecode provided", "Was unable to find Gamecode in queryparameter. Please try again.")
+            if (!gamecode.val || gamecode.val == '') {
+                failureSnackbarMessage(
+                    'No Gamecode provided',
+                    'Was unable to find Gamecode in queryparameter. Please try again.'
+                );
             }
 
-          if (!gameCodeRegex.test(gamecode)) {
-            failureSnackbarMessage("Error: Gamecode","The provided gamecode does not consist of 6 numbers.")
-            return;
-          }
-          try {
-            const recievedEndpointId = await getHostIdFromGamecode(gamecode);    
-            console.log("ENDPOINT: "+recievedEndpointId);
-            endpointId.val = recievedEndpointId.toString();
+            if (!gameCodeRegex.test(gamecode)) {
+                failureSnackbarMessage('Error: Gamecode', 'The provided gamecode does not consist of 6 numbers.');
+                return;
+            }
+            try {
+                const recievedEndpointId = await getHostIdFromGamecode(gamecode);
+                console.log('ENDPOINT: ' + recievedEndpointId);
+                endpointId.val = recievedEndpointId.toString();
 
-            successSnackbarMessage("Lobby joined","Joined Lobby successfully")
-            activeComponent.val = "nameSelection";
-          } catch (error) {
-            console.error("Error when attempting to find Lobby: "+error)
-            failureSnackbarMessage("Lobby not found", "There is no lobby for this gamecode.")
-          }
-
-      } catch (error) {
-          console.error("ERROR (joinGameByGamecode): " + error);
-          failureSnackbarMessage("Unable to Join","An error occured and it was not possible to join the game. Please try again later")
-      }
-  }
-  getEndpointByGamecode()
-
-  const joinGame = async () => {
-    activeComponent.val = "loading"
-
-    if (!gamecode || !gamecode.val || gamecode.val == "") {
-      return;
+                successSnackbarMessage('Lobby joined', 'Joined Lobby successfully');
+                activeComponent.val = 'nameSelection';
+            } catch (error) {
+                console.error('Error when attempting to find Lobby: ' + error);
+                failureSnackbarMessage('Lobby not found', 'There is no lobby for this gamecode.');
+            }
+        } catch (error) {
+            console.error('ERROR (joinGameByGamecode): ' + error);
+            failureSnackbarMessage(
+                'Unable to Join',
+                'An error occured and it was not possible to join the game. Please try again later'
+            );
+        }
     }
+    //getEndpointByGamecode();
 
-    const api: PlayerAPIType = await datex.get(`${endpointId.val}.PlayerAPI`)
-    if(!api) throw Error("Couldn't get Player API")
+    const joinGame = async () => {
+        try {
+            activeComponent.val = 'loading';
 
-    const res: {state: Datex.Pointer<string>; currentRound: Datex.Pointer<number>;} = await api.joinGame(name.val)
+            if (!gamecode || !gamecode.val || gamecode.val == '') {
+                return;
+            }
 
-    apiObj.playerApi = api
+            const api: PlayerAPIType = await datex.get(`${endpointId.val}.PlayerAPI`);
+            if (!api) throw Error("Couldn't get Player API");
 
-    currentRoundId.val = res.currentRound.id;
-    activeComponent.val = "liveGame";
-    stateId.val = res.state.id;
-  }
+            const res: { state: Datex.Pointer<string>; currentRound: Datex.Pointer<number> } = await api.joinGame(name.val);
 
-  const renderComponent = () => {
-    switch (activeComponent.val) {
-      case 'liveGame':
-        return <div>Fehler noch nicht implementiert<GameScreen stateId={stateId.val} currentRoundId={currentRoundId.val} apiObj={apiObj} /></div>;
-      case 'nameSelection':
-        return <div>
-          <h1>WHAT SHOULD WE CALL YOU?</h1>
-        <div class="name-input-container">
-            <input  type="text" 
-                    class="name-input" 
-                    id="nameInput" 
-                    value={name}
-                    maxlength="12" 
-                    placeholder="Enter your name"
-                    required />
-            <div class="glowing-line" id="glowingLine"></div>
-        </div>
-        <button type="button" class="button" onclick={() => joinGame()}>JOIN</button>
-          
-        </div>;
-      case 'loading':
-        return <LoadingScreen text={"Loading..."}/>;
-      default:
-        return <div>Es ist ein Fehler aufgetreten.</div>;
-    }
+            apiObj.playerApi = api;
 
-    
-  };  
+            currentRoundId.val = res.currentRound.id;
+            activeComponent.val = 'liveGame';
+            stateId.val = res.state.id;
+        } catch (error) {
+            console.error('ERROR: JoinScreen/joinGame: ' + error);
+            failureSnackbarMessage(
+                'Error when loading Lobby',
+                'Could not join the Lobby. Please make sure the Host is connected.'
+            );
+        }
+    };
 
-  return (
-      <main class="section">
-         {renderComponent()}
-        <Snackbar/>
-      </main>
-  );
+    const renderComponent = () => {
+        switch (activeComponent.val) {
+            case 'liveGame':
+                return <GameScreen stateId={stateId.val} currentRoundId={currentRoundId.val} apiObj={apiObj} />
+            case 'nameSelection':
+                return (
+                    <div>
+                        <h1>WHAT SHOULD WE CALL YOU?</h1>
+                        <div class="name-input-container">
+                            <input
+                                type="text"
+                                class="name-input"
+                                id="nameInput"
+                                value={name}
+                                maxlength="12"
+                                placeholder="Enter your name"
+                                required
+                            />
+                            <div class="glowing-line" id="glowingLine"></div>
+                        </div>
+                        <button type="button" class="button" onclick={() => joinGame()}>
+                            JOIN
+                        </button>
+                    </div>
+                );
+            case 'loading':
+                return <LoadingScreen text={'Loading...'} />;
+            default:
+                return <div> <p>We're sorry, something happend that was not supposed to happen. </p><button type="button" class="button" onclick={() => redirect('/')}>Go Back</button></div>;
+        }
+    };
+
+    return (
+        <main class="section">
+            {renderComponent()}
+            <Snackbar />
+        </main>
+    );
 })
 export default class JoinScreen extends Component<JoinScreenProps> {}
