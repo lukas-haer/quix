bd4efed814dc021b32d8818db5b3c282420d50a4
tkoengeter <tom.koengeter@adesso.de>
2025-06-22 12:24:37 +0200

    create wrapped entrypoint

--- frontend/entrypoint.tsx
+++ frontend/entrypoint.tsx
@@ -1,17 +1,9 @@
-import MainMenu from "./src/components/MainMenu/MainMenu.tsx";
-import HostDashboard from "./src/components/host/HostDashboard/HostDashboard.tsx";
-import JoinScreen from "./src/components/player/JoinScreen/JoinScreen.tsx";
-import {CreateQuiz} from './src/components/gamecreation/createQuiz/CreateQuiz.tsx';
-import Wrapper from "./Wrapper.tsx";
+import WrappedEntrypoint from "./wrappedEntrypoint.tsx";
 
 export default function Entrypoint() {
     return (
-      {
-          "/": () => <Wrapper><MainMenu/></Wrapper>,
-          "/join/:id": (_:any, {id}:{ id:string }) => <JoinScreen id={id}/>,
-          "/join": () => <JoinScreen/>,
-          "/create": () => <HostDashboard/>,
-          "/quiz": () => <CreateQuiz/>
-      }
-    );
+          <WrappedEntrypoint></WrappedEntrypoint>
+    )
 }
+
+
