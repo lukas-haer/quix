06ca98299ee59acac1ff25252736b5d6ddeba232
ama-manu <ama.manu@proton.me>
2025-06-25 00:50:43 +0200

    fix(HostFinishedScreen) prop types

--- frontend/src/components/host/HostFinishedScreen/HostFinishedScreen.tsx
+++ frontend/src/components/host/HostFinishedScreen/HostFinishedScreen.tsx
@@ -4,18 +4,18 @@ import {
   StateOptions,
 } from "../../../models/GameState.ts";
 
-@template(function () {
+@template(function ({state, currentRound} : {state: Datex.Pointer<StateOptions>, currentRound: Datex.Pointer<number>}) {
   return (
     <div>
       <h2>Game has finished.</h2>
 	  <button onclick={() => {
-		this.properties.currentRound.val = 0;
-		this.properties.state.val = "waiting";
-		}}>New Game</button>
+		currentRound.val = 0;
+		state.val = "waiting";
+		}}>New Round</button>
     </div>
   );
 })
 
 //TODO: proper prop types instead of any
-export class HostFinishedScreen extends Component<{state : Datex.Pointer<StateOptions> | any, currentRound: Datex.Pointer<number> | any}> {
+export class HostFinishedScreen extends Component<{state : Datex.Pointer, currentRound: Datex.Pointer}> {
 }
