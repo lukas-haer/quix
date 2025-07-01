c098c3244789c30815090a5e7ba6fc3b195b516f
lukas-haer <lukas.haertig@gmail.com>
2025-07-01 16:55:12 +0200

    added design for Player Waiting screen

--- frontend/src/components/utils/loadingscreen/LoadingScreen.tsx
+++ frontend/src/components/utils/loadingscreen/LoadingScreen.tsx
@@ -1,23 +1,22 @@
 import { Component, template } from 'uix/components/Component.ts';
-import { UIX } from "uix"
+import { UIX } from 'uix';
 
-@template((props) => {
+@template(props => {
+    UIX.Theme.useTheme('uix-light-plain');
 
-    UIX.Theme.useTheme("uix-light-plain")
-
-	const text = $("Loading...")
-	if (props.text) {
-		text.val = props.text;
-	}
+    const text = $('Loading...');
+    if (props.text) {
+        text.val = props.text;
+    }
 
     return (
-          <section class="loading-screen">
-    <div class="loading-text">Loading...</div>
-    <div class="shape rectangle"></div>
-    <div class="shape circle"></div>
-    <div class="shape triangle"></div>
-    <div class="shape star"></div>
+        <section class="loading-screen">
+            <div class="loading-text">{text}</div>
+            <div class="shape rectangle"></div>
+            <div class="shape circle"></div>
+            <div class="shape triangle"></div>
+            <div class="shape star"></div>
         </section>
     );
 })
-export class LoadingScreen extends Component<{text?: string}> {}
+export class LoadingScreen extends Component<{ text?: string }> {}
