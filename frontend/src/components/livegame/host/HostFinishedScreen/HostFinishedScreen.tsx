82964ef03c12fbd332b7c193d06e3e34573b676d
ama-manu <ama.manu@proton.me>
2025-07-01 21:49:38 +0200

    chore(HostFinishedScreen): moved file to fit new project structure

--- frontend/src/components/livegame/host/HostFinishedScreen/HostFinishedScreen.tsx
+++ frontend/src/components/livegame/host/HostFinishedScreen/HostFinishedScreen.tsx
@@ -2,7 +2,7 @@ import { Datex } from "datex-core-legacy/datex.ts";
 import { Component, template } from "uix/components/Component.ts";
 import {
   StateOptions,
-} from "../../../models/GameState.ts";
+} from "../../../../models/GameState.ts";
 
 @template(function ({state, currentRound} : {state: Datex.Pointer<StateOptions>, currentRound: Datex.Pointer<number>}) {
   return (
@@ -16,6 +16,5 @@ import {
   );
 })
 
-//TODO: proper prop types instead of any
 export class HostFinishedScreen extends Component<{state : Datex.Pointer, currentRound: Datex.Pointer}> {
 }
--- frontend/src/components/livegame/host/HostMain.tsx
+++ frontend/src/components/livegame/host/HostMain.tsx
@@ -5,6 +5,7 @@ import { Player, GameStateObjects, StateOptions } from "../../../models/GameStat
 import { JoinGameReturn, GetCurrentQuestionReturn } from "../../../models/PlayerApiReturns.ts";
 import { HostWaitingScreen } from "./HostWaitingScreen/HostWaitingScreen.tsx";
 import { HostPlayingScreen } from "./HostPlayingScreen/HostPlayingScreen.tsx";
+import { HostFinishedScreen } from "./HostFinishedScreen/HostFinishedScreen.tsx";
 import { Component, template } from "uix/components/Component.ts";
 import { Snackbar } from "frontend/src/components/utils/snackbar/Snackbar.tsx";
 
@@ -95,7 +96,7 @@ class PlayerAPI {
           state.val === "playing" && <HostPlayingScreen currentRound={currentRound.val} gameStateObjects={gameStateObjects} />
         }
         {
-          state.val === "finished" && <h2>Game has finished.</h2>
+          state.val === "finished" && <HostFinishedScreen state={state} currentRound={currentRound} />
         }
     </div>
     )
