b307fe07d3303097ecbd2753144299e417d2ef00
n3ling49 <noahdreiling@gmail.com>
2025-07-04 11:47:16 +0200

    refactor: reuse import quiz component in create quiz

--- frontend/src/components/livegame/host/QuizImport/QuizImport.tsx
+++ frontend/src/components/livegame/host/QuizImport/QuizImport.tsx
@@ -3,13 +3,36 @@ import { ObjectRef } from 'datex-core-legacy/runtime/pointers.ts';
 import { GameStateObjects, Player, StateOptions } from 'frontend/src/models/GameState.ts';
 import { successSnackbarMessage, failureSnackbarMessage } from "frontend/src/components/utils/snackbar/Snackbar.tsx";
 import { Separator } from "frontend/src/components/utils/Separator/Separator.tsx";
+import { Quiz } from "frontend/src/models/Quiz.ts";
 
 type QuizImportProps = {
 	gameStateObjects: ObjectRef<GameStateObjects>;
 }
 
+type ImportButtonProps = {
+	callback: (quiz: Quiz) => void;
+}
+
 @style("../HostMain.css")
 @template(({ gameStateObjects }: QuizImportProps) => {
+	//TODO: move me into setup screen instead of host waiting screen
+	const currentQuizName = $("")
+
+	return (
+		<div class="l-col l-col-6 center-vertically">
+			<Separator text={"Manually import quiz"} />
+			<span>{currentQuizName.val}</span>
+			<ImportButton callback={(quiz: Quiz) => {
+				gameStateObjects.questions = quiz.questions
+				currentQuizName.val = quiz.title
+			}}/>
+		</div>
+	);
+})
+export class QuizImport extends Component<QuizImportProps> {}
+
+@style("../HostMain.css")
+@template(({ callback }: ImportButtonProps) => {
 	//TODO: move me into setup screen instead of host waiting screen
 	const currentQuizName = $("")
 
@@ -24,23 +47,19 @@ type QuizImportProps = {
         const quizObject = JSON.parse(text)
         if(!quizObject) return failureSnackbarMessage("Error", "Failed to parse quiz file content.", 2000)
         
-		//TODO: check if quizObject is actually a valid quiz and display error message if that's not the case
+		//TODO: check if quizObject is actually a valid quiz. Currently a lazy check is implemented.
+		if (typeof quizObject !== "object" || !quizObject.questions) return failureSnackbarMessage("Error", "The file does not contain a valid quiz object.")
 
-		gameStateObjects.questions = quizObject.questions
-		currentQuizName.val = file.name
+		callback(quizObject)
         successSnackbarMessage("Success", "Quiz import succeeded.", 2000)
     }
 
 
 	return (
-		<div class="l-col l-col-6 center-vertically">
-			<Separator text={"Manually import quiz"} />
-			<span>{currentQuizName.val}</span>
 			<label class="button">
 				<input type="file" accept="application/json" id="import-quiz-input" style={{display: "none"}} onchange={handleImportQuiz}/>
 				Import Quiz
 			</label>
-		</div>
 	);
 })
-export class QuizImport extends Component<QuizImportProps> {}
+export class ImportButton extends Component<ImportButtonProps> {}
