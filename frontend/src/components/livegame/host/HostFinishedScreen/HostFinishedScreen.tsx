e3b18f4db6c3f14f7e5f840225013a79c5f5987b
Jonas <bielefeld@campus.tu-berlin.de>
2025-07-08 18:02:56 +0200

    designs inpllemented but not perfect yet

--- frontend/src/components/livegame/host/HostFinishedScreen/HostFinishedScreen.tsx
+++ frontend/src/components/livegame/host/HostFinishedScreen/HostFinishedScreen.tsx
@@ -5,21 +5,35 @@ import {
 } from "../../../../models/GameState.ts";
 
 type HostFinishedScreenProps = {
-    state: Datex.Pointer<StateOptions>;
-    currentRound: Datex.Pointer<number>;
+  state: Datex.Pointer<StateOptions>;
+  currentRound: Datex.Pointer<number>;
 };
 
-@template(function ({state, currentRound} : HostFinishedScreenProps) {
+@template(function ({ state, currentRound }: HostFinishedScreenProps) {
   return (
-    <div>
-      <h2>Game has finished.</h2>
-	  <button type="button" onclick={() => {
-		currentRound.val = 0;
-		state.val = "waiting";
-		}}>New Round</button>
-    </div>
+    <body>
+      <h1>🏆 Final Standings</h1>
+      <div class="podium">
+        <div class="place second">
+          <div class="name" id="secondName"></div>
+          <div class="score" id="secondScore"></div>
+        </div>
+        <div class="place first">
+          <div class="name" id="firstName"></div>
+          <div class="score" id="firstScore"></div>
+        </div>
+        <div class="place third">
+          <div class="name" id="thirdName"></div>
+          <div class="score" id="thirdScore"></div>
+        </div>
+      </div>
+      <button type="button" onclick={() => {
+        currentRound.val = 0;
+        state.val = "waiting";
+      }}>New Round</button>
+    </body>
   );
 })
 
-export class HostFinishedScreen extends Component<{state : Datex.Pointer, currentRound: Datex.Pointer}> {
+export class HostFinishedScreen extends Component<{ state: Datex.Pointer, currentRound: Datex.Pointer }> {
 }
