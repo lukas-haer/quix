6fa242322e0eef42078fe6f0a920434034980e5a
lukas-haer <lukas.haertig@gmail.com>
2025-07-17 14:09:09 +0200

    improvement(all): Smaller design changes and new QR Code module, since the old one lead to long loading times

--- frontend/src/components/utils/qrcode/qrcode.tsx
+++ frontend/src/components/utils/qrcode/qrcode.tsx
@@ -1,14 +1,39 @@
 import { Component, template } from "uix/components/Component.ts";
-//import QrCreator from 'https://cdn.jsdelivr.net/npm/qr-creator/dist/qr-creator.es6.min.js';
+import QRCodeStyling from 'https://cdn.jsdelivr.net/npm/qr-code-styling@1.9.2/+esm';
 
 @template((props) => {
-  const url = props.url  
+  const url = props.url;
+
+  const qrCode = new QRCodeStyling({
+    type: "canvas",
+    shape: "square",
+    width: 400,
+    height: 400,
+    data: url,
+    margin: 0,
+    qrOptions: {
+      typeNumber: 0,
+      mode: "Byte",
+      errorCorrectionLevel: "Q"
+    },
+    backgroundOptions: {
+      round: 10,
+      color: "#ffffff"
+    },
+    cornersSquareOptions: {
+      type: "extra-rounded",
+      color: "#000000"
+    },
+  });
+
+  const container = document.createElement("div");
+  qrCode.append(container);
 
   return (
     <div>
-
+      {container}
     </div>
-  )
+  );
 })
 
 export class QrCode extends Component<{ url: string }> {}
